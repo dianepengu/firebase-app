c6c1790fd6f0c922d55eb2b76067601480b7fc2a
Diane Pengu <lovely.sula@ciit.edu.ph>
2024-02-26 13:11:42 +0800

    added dotenv

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,21 @@
-import {initializeApp} from 'firebase/app'
-import {getFirestore} from 'firebase/firestore'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAKLOwO_V5zBzovvqUVOdkHo9JCxvlsvMc",
-    authDomain: "articledemo.firebaseapp.com",
-    projectId: "articledemo",
-    storageBucket: "articledemo.appspot.com",
-    messagingSenderId: "1084057524922",
-    appId: "1:1084057524922:web:f950c3af9d147285bc8083"
-  };
+  apiKey: REACT_APP_APT_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID
+};
 
-  initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-  const db = getFirestore();
-
-  export {db}
+export { db }
