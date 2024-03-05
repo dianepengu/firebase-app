9281ca9abc016371202f5cd229b92674cff3be59
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-05 18:35:32 +0800

    edited gap size

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -19,7 +19,7 @@ export default function Explore() {
 
     return (
         <div className="flex flex-col w-full h-full gap-5">
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-2">
                 <h1 className="text-2xl font-semibold dark:text-black">All Recipes</h1>
                 <p className="text-md dark:text-gray-400">These are recipes posted by various users</p>
                 <div className="flex flex-wrap w-full gap-5">
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,7 @@
 export default function Favorites() {
     return (
         <div className="flex w-full h-full gap-5">
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-2">
                 <h1 className="text-2xl font-semibold dark:text-black">All Recipes</h1>
                 <p className="text-md dark:text-gray-400">These are recipes that you have favorited</p>
             </div>
